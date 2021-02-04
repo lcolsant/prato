fb8732721ee116d262c570acbf5fc2372e1c8d2e
Lee Colsant <lee.colsant@gmail.com>
2021-02-03 22:31:23 -0800

    responsive navbar

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9666,7 +9666,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49522" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49596" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
